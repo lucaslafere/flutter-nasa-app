c1aaa42128ca38e144a1ad15a41aa7ae46e106eb
Lucas Lafèré <laferelucas@gmail.com>
2023-09-24 22:25:40 -0300

    fix: env missing import

--- lib/config/api_constants.dart
+++ lib/config/api_constants.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class ApiConstants {
   static const String BASE_URL = 'https://api.nasa.gov/planetary/';
   static const String APOD_ENDPOINT = 'apod';
-  static const String API_KEY = dotenv.env['NASA_API_KEY']!;
+  String apiKey = dotenv.env['NASA_API_KEY']!;
 }
