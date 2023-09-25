1bb45ef904a48de38b4f7f8eec10cfe944096fdf
Lucas Lafèré <laferelucas@gmail.com>
2023-09-24 22:31:20 -0300

    chore: fix dependencies

--- lib/config/api_constants.dart
+++ lib/config/api_constants.dart
@@ -3,5 +3,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class ApiConstants {
   static const String BASE_URL = 'https://api.nasa.gov/planetary/';
   static const String APOD_ENDPOINT = 'apod';
-  String apiKey = dotenv.env['NASA_API_KEY']!;
+  String API_KEY = dotenv.env['NASA_API_KEY']!;
 }
